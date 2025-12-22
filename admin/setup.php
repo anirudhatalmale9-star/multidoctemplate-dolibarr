c389dbf56446067350ed2c453da76e3665b1909e
Anirudha Talmale <anirudhatalmale9@gmail.com>
2025-12-22 16:41:44 +0000

    Fix main.inc.php path resolution for different Dolibarr installations

--- admin/setup.php
+++ admin/setup.php
@@ -1,6 +1,71 @@
 <?php
-require '../main.inc.php';
+/* Copyright (C) 2024
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
-llxHeader();
-print '<h2>MultiDocTemplate – Setup</h2>';
+/**
+ * \file       admin/setup.php
+ * \brief      MultiDocTemplate setup page
+ */
+
+// Load Dolibarr environment
+$res = 0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
+    $res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+}
+// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+    $i--;
+    $j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
+    $res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
+}
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
+    $res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
+}
+// Try main.inc.php using relative path
+if (!$res && file_exists("../../main.inc.php")) {
+    $res = @include "../../main.inc.php";
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+    $res = @include "../../../main.inc.php";
+}
+if (!$res) {
+    die("Include of main fails");
+}
+
+global $langs, $user;
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT."/core/lib/admin.lib.php";
+
+// Translations
+$langs->loadLangs(array("admin", "multidoctemplate@multidoctemplate"));
+
+// Access control
+if (!$user->admin) {
+    accessforbidden();
+}
+
+/*
+ * View
+ */
+
+llxHeader('', $langs->trans('MultiDocTemplateSetup'));
+
+print load_fiche_titre($langs->trans('MultiDocTemplateSetup'), '', 'title_setup');
+
+print '<div class="info">'.$langs->trans('MultiDocTemplateSetupDesc').'</div>';
+
 llxFooter();
+$db->close();
